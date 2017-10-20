69a479a9d4fca7d0766bce35b5fb02a914145ffa
Gabriel De Ioannes <gdeioannes@gmail.com>
2017-10-20 12:34:35 -0300

    New UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+	public GameObject vrBtn;
+	public GameObject optionBtn;
+
+	// Use this for initialization
+	void Start () {
+		hidePanel();
+	}
+
+	public void showPanel(){
+		optionBtn.SetActive(true);
+		vrBtn.SetActive(false);
+	}
+
+	public void hidePanel(){
+		optionBtn.SetActive(false);
+		vrBtn.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
